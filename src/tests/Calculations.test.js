49a88534a05f20304083483a6632e758af3e770f
Oksana Nedilko <nedilko7b@gmail.com>
2022-12-13 20:58:19 +0200

    added commented import to demostrate calc tests

--- src/tests/Calculations.test.js
+++ src/tests/Calculations.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Calculations from "../components/Calculations";
+// import Calculations from "../brokenComponents/Calculations";
 
 describe("Calculations", () => {
   test("renders correctly", () => {
